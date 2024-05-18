1a68bd1969b0aa138282eb3aa1a47a8ef30c6b1d
Maxim <maxim.khachatryan.2013@gmail.com>
2024-05-18 23:37:41 +0400

    Add db context

--- GentleExpress.API/Program.cs
+++ GentleExpress.API/Program.cs
@@ -1,3 +1,6 @@
+using GentleExpress.Repositories.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<GentleExpressPostgresDbContext>(opt =>
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("GentleExpressDbConnection")));
 
 var app = builder.Build();
 
---
+++ GentleExpress.Repositories.EfCore/GentleExpressPostgresDbContext.cs
@@ -0,0 +1,29 @@
+﻿using GentleExpress.Common.EfCore;
+using GentleExpress.Repositories.Abstraction.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GentleExpress.Repositories.EfCore;
+
+public class GentleExpressPostgresDbContext : GentleExpressBaseDbContext
+{
+    public GentleExpressPostgresDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<ClientAddress> ClientAddresses { get; set; }
+    public DbSet<ClientUser> ClientUsers { get; set; }
+    public DbSet<Courier> Couriers { get; set; }
+    public DbSet<District> Districts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql();
+    }
+}
