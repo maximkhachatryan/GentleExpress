3799a243b71716985588d4cbc487ec3a0b197fc0
Maxim <maxim.khachatryan.2013@gmail.com>
2024-05-26 20:38:58 +0400

    Cleanup

--- GentleExpress.API/Controllers/CitiesController.cs
+++ GentleExpress.API/Controllers/CitiesController.cs
@@ -2,7 +2,6 @@
 using GentleExpress.Services.Abstraction.Services;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GentleExpress.API.Controllers;
 
